c68301aa96ea35fb439bd37d43e14a3dd0206388
mstriso <128740248+mstriso@users.noreply.github.com>
2023-12-20 13:41:10 +0100

    Add files via upload

--- console_components/docker_restart_watchdog.sh
+++ console_components/docker_restart_watchdog.sh
@@ -4,7 +4,7 @@ while true; do
     # Check if the container is running
     if [ "$(docker inspect -f '{{.State.Running}}' test)" = "false" ]; then
         sleep 900  # Wait for 15 minutes
-        docker start test
+        docker start console
     fi
     sleep 60  # Check every 60 seconds
 done
